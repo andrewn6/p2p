185c996518c3e32d668d6b9f233846390f359f92
Andrew <andrewnijmeh1@gmail.com>
2022-04-23 21:25:28 -0400

    connection class

---
+++ network/connect.py
@@ -0,0 +1,18 @@
+class Connect(object):
+  def __init__(self, channel, node_position node_ip):
+    self.channel = channel
+    self.node_position
+    self.node_ip = node_ip
+
+  @property
+  def channel(self):
+    return self._channel
+
+  @property
+  def node_ip(self):
+    return self.node_ip
+
+  @property
+  def node_position
+    return self.node_position
+
