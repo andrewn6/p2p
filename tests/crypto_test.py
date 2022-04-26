0364e8ddbfbf20dd39b2905352b41e7d06961fbf
Necloremius <63004866+Necloremius@users.noreply.github.com>
2022-04-26 11:06:26 -0400

    Added tracemalloc + timeit for benchmarking purposes

--- tests/crypto_test.py
+++ tests/crypto_test.py
@@ -1,13 +1,19 @@
 import random
 import string
-from crypto import *
-
-# printing lowercase
+import tracemalloc
+import timeit
+from main import *
 
 count = 0
 num = int(input("How many test cases would you like to be ran? "))
 
+# Initialize benchmarking
+tracemalloc.start()
+start = timeit.default_timer()
+
+# Tests
 for _ in range(num):
+
     letters = string.ascii_lowercase
     testcase = (''.join(random.choice(letters) for i in range(10)))
 
@@ -24,4 +30,31 @@ for _ in range(num):
         count += 1
 
 
-print(count)
+end = timeit.default_timer()
+memorg = tracemalloc.get_traced_memory()
+
+# Test results
+print(f"\n{count} successful tests out of {num} total.\nPercentage: {(num/count)*100:.5f}")
+
+# Benchmarking
+print(f"Time elapsed: {end-start}\nAverage time per test: {(end-start)/count}")
+print(f"Total used memory: {(memorg[0] + memorg[1])/2}\nAverage memory used per test: {(memorg[0] + memorg[1])/(2*num)}")
+
+tracemalloc.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
