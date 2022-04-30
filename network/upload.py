01b23662f65c006c19de2f490dbb806d6eed26e4
Necloremius <63004866+Necloremius@users.noreply.github.com>
2022-04-29 20:49:05 -0400

    Update upload.py

--- network/upload.py
+++ network/upload.py
@@ -1,12 +1,17 @@
 # Functionality for uploading a file to the HTTP server
 import os
-
+from crypto import *
 fileitem = form['filename']
 
 if fileitem.filename:
     fn = os.path.basename(fileitem.filename)
 
-    open(fn, 'wb').write(fileitem.file.read())
+    open(fn, 'wb').write(encrypt(fileitem.file.read()))
+
+
+
+
+
 
 
 
